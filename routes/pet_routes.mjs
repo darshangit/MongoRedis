7c731c61dbfa881c0ad3e9d1f9f15bd76827c06f
darshangit <darshans1989@gmail.com>
2018-06-21 21:34:32 +0530

    final touches

--- routes/pet_routes.mjs
+++ routes/pet_routes.mjs
@@ -9,70 +9,62 @@ asd.defaults({
 let client = redis.createClient(6379,'127.0.0.1');
 
 export default function(app) {
-  app.get('/pets', function(req, res) {
-    async.parallel(
-      {
-        cat: function(callback) {
-          asd(
-            {
-              uri: 'http://localhost:3000/cat'
-            },
-            function(error, response, body) {
-              if (error) {
-                callback({ service: 'cat', error: error });
-                return;
-              }
-              if (!error && response.statusCode === 200) {
-                callback(null, body);
-              } else {
-                callback(response.statusCode);
-              }
-            }
-          );
-        },
-        dog: function(callback) {
-          client.get('http://localhost:3001/dog', function(error, dog) {
-            if(error) {throw error;};
-            if(dog) {
-              callback(null, JSON.parse(dog));
-            }else{
-              asd({
-                  uri: 'http://localhost:3001/dog'
-                },
-                function(error, response, body) {
-                  if (error) {
-                    callback({ service: 'dog', error: error });
-                    return;
-                  }
-                  if (!error && response.statusCode === 200) {
-                    callback(null, body.data);
-                    client.set('http://localhost:3001/dog', JSON.stringify(body.data), function(error) {
-                      if(error) { throw error;};
-                    });
-                  } else {
-                    callback(response.statusCode);
-                  }
-                }
-              );
-            }
-          })
-        }
-      },
-      function(error, results) {
-          var y = 0;
-          for(var x=0;x<10000000000000;x++){
-                y = y + x;
-          }
-          console.log(y)
-        res.json({
-          error: error,
-          results: results
-        });
-      }
-    );
-  });
+  /* Read */
+  app.get('/pets', function (req, res) {
 
-  app.get('/ping', function(req, res) {
-      res.json({pong: Date.now()});
-  });
+    async.parallel({
+        cat: function(callback){
+            asd({uri: 'http://localhost:3000/cat'}, function(error, response, body) {
+                if (error) {
+                    callback({service: 'cat', error: error});
+                    return;
+                };
+                if (!error && response.statusCode === 200) {
+                    callback(null, body.data);
+                } else {
+                    callback(response.statusCode);
+                }
+            });
+        },
+        dog: function(callback){
+
+            client.get('http://localhost:3001/dog', function(error, dog) {
+                if (error) {throw error;};
+                if (dog) {
+                    callback(null, JSON.parse(JSON.stringify(dog)));
+                } else {
+
+                    asd({uri: 'http://localhost:3001/dog'}, function(error, response, body) {
+                        if (error) {
+                            callback({service: 'dog', error: error});
+                            return;
+                        };
+                        if (!error && response.statusCode === 200) {
+                            // client.set('http://localhost:3001/dog', JSON.stringify(body.data), function (error) {
+                              client.set('http://localhost:3001/dog', JSON.stringify(body.data), function (error) {
+                                if (error) {throw error;};
+                            });
+                            callback(null, body.data);
+                        } else {
+                            callback(response.statusCode);
+                        }
+                    });
+
+                }
+            });
+
+        }
+    },
+    function(error, results) {
+        res.json({
+            error: error,
+            results: results
+        });
+    });
+
+});
+
+app.get('/ping', function (req, res) {
+    res.json({pong: Date.now()});
+});
 }
